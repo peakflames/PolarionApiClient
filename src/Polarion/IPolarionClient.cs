1ee1b799b6110c5998641062f9e6ba1cddde0eda
Todd Schavey <tschavey@archer.com>
2025-03-08 00:01:11 -0500

    updated IPolarionClient.cs

--- src/Polarion/IPolarionClient.cs
+++ src/Polarion/IPolarionClient.cs
@@ -10,5 +10,6 @@ public interface IPolarionClient
     Task<Result<WorkItem>> GetWorkItemByIdAsync(string workItemId);
     Task<Result<WorkItem[]>> SearchWorkitem(string query, string order, List<string> field_list);
     Task<Result<WorkItem[]>> SearchWorkitemInBaseline(string baselineRevision, string query, string order, List<string> field_list);
+    Task<Result<Module[]>> GetDocumentsInSpace(string spaceName);
 }
 
